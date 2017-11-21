e7ed49540e48178648886f0c3995e9ba55153231
Mayara Machado <maaih18@gmail.com>
2017-11-20 22:43:25 -0200

    Adicionado funcionalidade de criar novo topico

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404
 from .models import *
 
@@ -13,4 +14,25 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk = pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first() 
+
+        topic = Topic.objects.create( subject = subject,
+                                      board = board,
+                                      starter = user
+                                      )
+
+
+        post = Post.objects.create( message = message,
+                                    topic = topic,
+                                    created_by = user
+                                    )
+
+
+        return redirect('board_topics', pk=board.pk)
+
     return render(request,'new_topic.html', {'board': board})
